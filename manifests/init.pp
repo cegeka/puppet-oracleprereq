9a8677451fb3b9e7b455de7b3f0c9f160a3c7062
Kristof Willaert <kristof.willaert@gmail.com>
2012-03-15 10:54:03 +0100

    Initial repository layout for puppet module oracleprereq

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: oracleprereq
+#
+# This module manages oracleprereq
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class oracleprereq {
+
+
+}
