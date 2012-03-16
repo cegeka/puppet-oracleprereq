e38da66f5ae541283b91ba83ef7b63c30ff9cfae
fdammeke <fabian.dammekens@cegeka.be>
2012-03-16 12:22:10 +0100

    Fixed rspec

--- spec/classes/oracleprereq_spec.rb
+++ spec/classes/oracleprereq_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe 'oracleprereq' do
-  let(:pre_condition) { "class limits {}" }
-  let(:pre_condition) { "define limits::conf($domain, $type, $item, $value) {}" }
+  let(:pre_condition) { "define limits::conf($domain, $type, $item, $value) {}" "class limits {}" }
+  #let(:pre_condition) { "class limits {}" }
   it { should contain_class 'oracleprereq' }
 end
