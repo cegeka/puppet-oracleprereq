14303959d91e83057c9b7bedcf87c56eb3b81579
fdammeke <fabian.dammekens@cegeka.be>
2012-03-22 13:50:08 +0100

    Fixed rspec architecture fact

--- spec/classes/oracleprereq_spec.rb
+++ spec/classes/oracleprereq_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 
 describe 'oracleprereq' do
   let(:pre_condition) { "define limits::conf($domain, $type, $item, $value) {}" "class limits {}" }
+  let(:facts) { { :architecture => 'i386' } }
   #let(:pre_condition) { "class limits {}" }
   it { should contain_class 'oracleprereq' }
 end
